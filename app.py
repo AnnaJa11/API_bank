f05514fdbb73aa86a0c1ba897efdb6ec1672259f
Anna J <ajasion5@gmail.com>
2024-01-15 18:40:30 +0000

    write API data to csv file

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import requests
 import json
-
+import csv
 
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-data = response.json()
+data_API = response.json()
+my_dict = dict(zip(range(len(data_API)), data_API))
 
-my_dict = dict(zip(range(len(data)), data))
-print(type(my_dict))
+dict_data = (my_dict[0]['rates'])
 
-rates = (my_dict[0]['rates'])
-print(rates)
+csv_columns = []
+for i in dict_data[0]:
+    csv_columns.append(i)
+print(csv_columns)
+
+with open("bank_rates.csv", 'w', encoding='utf8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+        writer.writeheader()
+        for data in dict_data:
+            writer.writerow(data)
