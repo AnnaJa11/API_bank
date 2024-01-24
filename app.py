10fdf84acf61dffd6a89ef368e7ece2639425b40
Anna J <ajasion5@gmail.com>
2024-01-24 23:50:07 +0000

    post currency code and user amount

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
 data_API = response.json()
 my_dict = dict(zip(range(len(data_API)), data_API))
-
-
 dict_data = (my_dict[0]['rates'])
+
+# dict_data = [{'currency': 'dolar amerykański', 'code': 'USD', 'bid': 4.0042, 'ask': 4.085}, {'currency': 'dolar australijski', 'code': 'AUD', 'bid': 2.6332, 'ask': 2.6864}, {'currency': 'dolar kanadyjski', 'code': 'CAD', 'bid': 2.9703, 'ask': 3.0303}, {'currency': 'euro', 'code': 'EUR', 'bid': 4.3457, 'ask': 4.4335}, {'currency': 'forint (Węgry)', 'code': 'HUF', 'bid': 0.011261, 'ask': 0.011489}, {'currency': 'frank szwajcarski', 'code': 'CHF', 'bid': 4.5965, 'ask': 4.6893}, {'currency': 'funt szterling', 'code': 'GBP', 'bid': 5.0773, 'ask': 5.1799}, {'currency': 'jen (Japonia)', 'code': 'JPY', 'bid': 0.026992, 'ask': 0.027538}, {'currency': 'korona czeska', 'code': 'CZK', 'bid': 0.1748, 'ask': 0.1784}, {'currency': 'korona duńska', 'code': 'DKK', 'bid': 0.5827, 'ask': 0.5945}, {'currency': 'korona norweska', 'code': 'NOK', 'bid': 0.3806, 'ask': 0.3882}, {'currency': 'korona szwedzka', 'code': 'SEK', 'bid': 0.3821, 'ask': 0.3899}, {'currency': 'SDR (MFW)', 'code': 'XDR', 'bid': 5.3134, 'ask': 5.4208}]
 
 csv_columns = []
 for i in dict_data[0]:
@@ -22,44 +22,32 @@ with open("bank_rates.csv", 'w', encoding='utf8') as csvfile:
         for data in dict_data:
             writer.writerow(data)
 
+codes = []
+for i in dict_data:
+    code = (i['code'])
+    codes.append(code)
+# codes = ['USD', 'AUD', 'CAD', 'EUR', 'HUF', 'CHF', 'GBP', 'JPY', 'CZK', 'DKK', 'NOK', 'SEK', 'XDR']
+asks = []
+for i in dict_data:
+    ask = (i['ask'])
+    asks.append(ask)
+# asks = [4.085, 2.6864, 3.0303, 4.4335, 0.011489, 4.6893, 5.1799, 0.027538, 0.1784, 0.5945, 0.3882, 0.3899, 5.4208]
      
 @app.route('/')
-def base_templ():
+def home():
     return render_template('base.html')
 
 @app.route("/base/calc", methods=['GET', 'POST'])
-def test():
-    if request.method == 'GET':
-       print("We received GET from CALC")
-       return render_template("calculator.html", data=dict_data)
-    elif request.method == "POST":
-        form_data = request.form
-        print(form_data)
-        select = form_data.get('user_select')
-        user_value = form_data.get('user_amount')
-        return select, user_value    # just to see what is there
-    return redirect("/")
-   
-dict_data_length = len(dict_data)
-currency_codes = []
-for i in range (0, dict_data_length):
-    code = dict_data[i]['code']
-    currency_codes.append(code)      
-print(currency_codes)
-   
-currency_ask_values = []
-for i in range (0, dict_data_length):
-    ask_value = dict_data[i]['ask']
-    currency_ask_values.append(ask_value)
-print(currency_ask_values)
-
-user_code_choice = input('Choose index  of currency: ')
-user_value = float(input('Enter your value: '))
-code_index = currency_codes.index(user_code_choice)
-code_ask_value = currency_ask_values[code_index]
-print(code_ask_value)
-result = round(user_value * code_ask_value, 2)
-print(f'{result} PLN')
+def calc():
+    payment = ''
+    if request.method == ['POST']:
+       amount = request.form.get("amount")
+       user_select = request.form.get("code")
+       for item in dict_data:
+          if item['code'] == user_select:
+            payment = round((float(item['ask']) * float(amount)), 2)
+    return render_template("calculator.html", codes=codes, payment=payment)
+ 
 
 if __name__ == "__main__":
     app.run(debug=True)
